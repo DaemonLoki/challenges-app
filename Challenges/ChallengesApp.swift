6bc9273017b3c9f01dbcd8ea21853dafea354b93
Stefan Blos <stefan.blos@codecamp-n.com>
2020-11-03 21:00:05 +0100

    Initial Commit

---
+++ Challenges/ChallengesApp.swift
@@ -0,0 +1,20 @@
+//
+//  ChallengesApp.swift
+//  Challenges
+//
+//  Created by Stefan Blos on 03.11.20.
+//
+
+import SwiftUI
+
+@main
+struct ChallengesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
