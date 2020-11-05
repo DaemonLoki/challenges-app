905c324b0e2f6b5d9f8fd8361b28e8fe7819952c
Stefan Blos <stefan.blos@codecamp-n.com>
2020-11-05 22:54:02 +0100

    show total count of actions sum on ChallengeView

--- Challenges/ChallengeView.swift
+++ Challenges/ChallengeView.swift
@@ -9,17 +9,34 @@ import SwiftUI
 
 struct ChallengeView: View {
     
+    @Environment(\.managedObjectContext) private var viewContext
+    
     var challenge: Challenge
+    
+    var totalCount: Double {
+        return challenge.actions?.array
+            .compactMap { $0 as? Action }
+            .map { $0.count }
+            .reduce(0.0, { accumulated, nextValue in
+                accumulated + nextValue
+            })
+            ?? 0.0
+    }
     
     var body: some View {
         VStack {
             Text("Current value")
-            Text("\(challenge.actions?.count ?? 0)")
+            Text("\(Int(totalCount))")
                 .font(.largeTitle)
             Spacer()
             
             Button(action: {
-                // TODO action
+                let action = Action(context: viewContext)
+                action.count = 20
+                action.date = Date()
+                action.challenge = challenge
+                
+                try? viewContext.save()
             }, label: {
                 Text("Add 20 Pushups")
             })
