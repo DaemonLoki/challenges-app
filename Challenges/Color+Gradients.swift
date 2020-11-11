322619a751451d1d0d0e5d485de9cd8a19213521
Stefan Blos <stefan.blos@codecamp-n.com>
2020-11-11 20:56:44 +0100

    added extensions for the gradient of the logo

---
+++ Challenges/Color+Gradients.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Gradients.swift
+//  Challenges
+//
+//  Created by Stefan Blos on 11.11.20.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static var gradientStart: Color = Color(#colorLiteral(red: 0.968627451, green: 0.7098039216, blue: 0, alpha: 1))
+    static var gradientMiddle: Color = Color(#colorLiteral(red: 0.7137254902, green: 0.1254901961, blue: 0.8784313725, alpha: 1))
+    static var gradientEnd: Color = Color(#colorLiteral(red: 0.1960784314, green: 0.7725490196, blue: 1, alpha: 1))
+    
+}
---
+++ Challenges/LinearGradient.swift
@@ -0,0 +1,14 @@
+//
+//  LinearGradient.swift
+//  Challenges
+//
+//  Created by Stefan Blos on 11.11.20.
+//
+
+import SwiftUI
+
+extension LinearGradient {
+    
+    static var logoGradient = LinearGradient(gradient: Gradient(colors: [.gradientStart, .gradientMiddle, .gradientEnd]), startPoint: .bottomLeading, endPoint: .topTrailing)
+    
+}
